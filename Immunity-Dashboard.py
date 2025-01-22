3e85966a070acd72d3d9711bd948f8138871a0f3
JOÃO CARVALHO <dev.joaocarvalho@gmail.com>
2025-01-22 11:28:54 -0300

    Update Immunity-Dashboard.py

--- Immunity-Dashboard.py
+++ Immunity-Dashboard.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 st.set_page_config(
     page_title="Immunity",
     page_icon="😷",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="auto"
 )
 
